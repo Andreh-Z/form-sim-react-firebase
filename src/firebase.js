ba2bf6a2bea8fca9b131d6c33ef1527d125c3bd4
Andreh-Z <witecan931@gmail.com>
2023-01-05 14:28:30 -0300

    dotEnv

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "@firebase/firestore";
+
+// Variables
+const apiKey = process.env.API_KEY;
+const authDomain = process.env.AUTH_DOMAIN;
+const projectID = process.env.PROJECT_ID;
+const Storage_bucket = process.env.STORAGE_BUCKET;
+const messaging_sender_id = process.env.MESSAGING_SENDER_ID;
+const app_id = process.env.APP_ID;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmxGoJ-tpysS17_5yqqX3D702w6gKNDqo",
-  authDomain: "greydive-bd.firebaseapp.com",
-  projectId: "greydive-bd",
-  storageBucket: "greydive-bd.appspot.com",
-  messagingSenderId: "666934268809",
-  appId: "1:666934268809:web:54613bcddd8dc1b7a18758",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectID,
+  storageBucket: Storage_bucket,
+  messagingSenderId: messaging_sender_id,
+  appId: app_id,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
