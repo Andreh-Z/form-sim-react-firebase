4024ed3a1cd223a18f61b7086613c0691d69abcc
Andreh-Z <witecan931@gmail.com>
2023-01-05 13:46:28 -0300

    fire_base_connect

--- src/Components/EncuestaForm.jsx
+++ src/Components/EncuestaForm.jsx
@@ -1,5 +1,141 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 export default function EncuestaForm() {
-  return <div>EncuestaForm</div>;
+  const [data, setData] = useState();
+  const [input, setInput] = useState({
+    full_name: "",
+    email: "",
+    birth_date: "",
+    country_of_origin: "",
+    terms_and_conditions: false,
+  });
+
+  async function fetchData() {
+    try {
+      const response = await fetch("/db.json");
+      const data = await response.json();
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+
+    if (type === "checkbox") {
+      setInput({
+        ...input,
+        [name]: checked,
+      });
+    } else {
+      setInput({
+        ...input,
+        [name]: value,
+      });
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(input);
+    // Envia el estado input al servidor
+    fetch("/enviar", {
+      method: "POST",
+      body: JSON.stringify(input),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      {data &&
+        data.items.map((item) => {
+          if (item.type === "text") {
+            return (
+              <div>
+                <label>{item.label}</label>
+                <input
+                  type={item.type}
+                  name={item.name}
+                  required={item.required}
+                  value={input[item.name]}
+                  onChange={(e) => handleChange(e)}
+                />
+              </div>
+            );
+          } else if (item.type === "email") {
+            return (
+              <div>
+                <label>{item.label}</label>
+                <input
+                  type={item.type}
+                  name={item.name}
+                  required={item.required}
+                  value={input[item.name]}
+                  onChange={(e) => handleChange(e)}
+                />
+              </div>
+            );
+          } else if (item.type === "date") {
+            return (
+              <div>
+                <label>{item.label}</label>
+                <input
+                  type={item.type}
+                  name={item.name}
+                  required={item.required}
+                  value={input[item.name]}
+                  onChange={(e) => handleChange(e)}
+                />
+              </div>
+            );
+          } else if (item.type === "select") {
+            return (
+              <div>
+                <label>{item.label}</label>
+                <select
+                  name={item.name}
+                  required={item.required}
+                  value={input[item.name]}
+                  onChange={(e) => handleChange(e)}
+                >
+                  {item.options.map((option) => {
+                    return <option value={option.value}>{option.label}</option>;
+                  })}
+                </select>
+              </div>
+            );
+          } else if (item.type === "checkbox") {
+            return (
+              <div>
+                <label>{item.label}</label>
+                <input
+                  type={item.type}
+                  name={item.name}
+                  required={item.required}
+                  checked={input[item.name]}
+                  onChange={(e) => handleChange(e)}
+                />
+              </div>
+            );
+          } else if (item.type === "submit") {
+            return (
+              <div>
+                <input type={item.type} value={item.label} />
+              </div>
+            );
+          }
+        })}
+    </form>
+  );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmxGoJ-tpysS17_5yqqX3D702w6gKNDqo",
+  authDomain: "greydive-bd.firebaseapp.com",
+  projectId: "greydive-bd",
+  storageBucket: "greydive-bd.appspot.com",
+  messagingSenderId: "666934268809",
+  appId: "1:666934268809:web:54613bcddd8dc1b7a18758",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
